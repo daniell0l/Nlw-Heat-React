4dcb6e8933e0d2a56cd531a5d425c0a42fc509f8
Daniel <daniel_l0l@hotmail.com>
2021-10-27 14:23:11 -0300

    feat: teste

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -4,7 +4,7 @@ import logoImg from '../../assets/logo.svg';
 export function MessageList() {
   return (
     <div className={styles.messageListWrapper}>
-      <img src={logoImg} alt="DoWhile 2021" />
+      <img src={logoImg} alt="DoWhile-2021" />
 
       <ul className={styles.messageList}>
       <li className={styles.message}>
